0ee68278cfc3d13f333acc8a4fdc002285ba550e
Khánh Sky <108038293+khanhdzx@users.noreply.github.com>
2022-08-17 09:58:10 +0700

    Add files via upload

---
+++ vip3/vip4/app.js
@@ -0,0 +1,146 @@
+
+
+// console.log(document);
+// let div = document.getElementById("app");
+// console.log(div);
+
+
+
+// // lấy thông qua classname
+// // trả về mảng Html
+// let divs = document.getElementsByClassName("app");
+// console.log(divs);
+// console.log(div[0]);
+// console.log(div[1]);
+// for(let i=0;i<=div.length-1;i++){
+// console.log(i,div[i]);
+// }
+
+// // .getElementsByTagName(tagname)
+// let dic = document.getElementsByTagName("div");
+// console.log(dic)
+
+// let div = document.getElementById("app");
+// console.log(div);
+// console.dir(div);
+// Basic properties (thuộc tính cơ bản)
+// .indexText
+// .indexHTML
+// .indexContent
+// div.innerText="hello everybody,my name is Peter";
+// div.innerHTML=`<b>Khánh Sky </b>`;
+// div.textContent;
+
+
+
+// .style
+// console.log(div.style)
+// div.style.backgroundColor = "green";
+// div.style.width="500px";
+// div.style.height="500px";
+// div.style.borderRadius="50%";
+// div.style.textAlign="center";
+// div.style.lineHeight="500px";
+// div.style.fontSize="100px";
+// div.style.fontWeight="bold";
+// div.style.color="pink";
+
+
+// .classList
+// console.log(div.classList);
+// div.classList.add("demo")
+
+
+// .classList.remove
+
+// console.log(div.classList);
+// div.classList.remove("demo")
+
+// div.classList.value= "add";
+// console.log(div.classList);
+// div.classList.value+=" demo";
+
+//id
+// console.log(div.id);
+
+
+// getAttibute
+// console.log(div.getAttribute("class"));
+// console.log(div.getAttribute("id"));
+
+//setAttribute
+// div.setAttribute("id","add2");
+// console.log(div.id)
+
+
+// document.createElement
+// let a= document.createElement("a");
+// a.setAttribute("href","https://www.google.com");
+// a.innerText="Click Me!"
+// console.log(a);
+
+//.appendChid()
+
+// div.appendChild(a);
+
+
+//.remove()
+// a.remove();
+
+
+//.children, .parentElement
+// console.log(div.children);//trả về mảng
+// console.log(div.children[0]);
+// let i= div.children[0];
+// console.log(i.parentElement.parentElement.parentElement);
+
+
+//Sự kiện Event
+// let btn =document.getElementById("vip");
+// btn.onclick = function(){
+//     div.style.backgroundColor="green";
+//     // alert("Chào anh khánh Sky")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
